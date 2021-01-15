bcc165d66500adb8570944236cf9488d1f4af5d9
Ahmad Faizal B H <ab4953@columbia.edu>
2021-01-15 20:11:07 +0530

    fix cornercases

--- cardboard.py
+++ cardboard.py
@@ -10,8 +10,10 @@ def cardboard_intersection(cardboards):
                     if h<=h_:
                         if x2<x_2 or j>i:
                             bottom = False
-                    elif bottom_pos<h_:
+                    elif x2!=x_2 and bottom_pos<h_:
                         bottom_pos=h_
+                    elif x2==x_2 and j>i:
+                        bottom = False
                 if x_1<=x1<=x_2 and h<=h_:
                     if x_1<x1 or h<h_ or j>i:
                         top = False
@@ -26,20 +28,42 @@ def cardboard_intersection(cardboards):
 
 
 
-
-
 if __name__ == "__main__":
+    
     cardboards1 = [(1,5,10),(4,6,8),(10,15,10),(11,12,8)]
     print("Input 1:", cardboards1)
     print("Output 1:", cardboard_intersection(cardboards1))
+
+    print("-"*30)
     
     cardboards2 = [(1,10,4),(1,8,6),(1,6,8)]
     print("Input 2:", cardboards2)
     print("Output 2:", cardboard_intersection(cardboards2))
     
+    print("-"*30)
+    
     cardboards3 = [(0,6,2),(5,10,8),(7,8,12)]
     print("Input 3:", cardboards3)
     print("Output 3:", cardboard_intersection(cardboards3))
+    
+    print("-"*30)
+    
+    cardboards4 = [(0,6,2)]
+    print("Input 4:", cardboards4)
+    print("Output 4:", cardboard_intersection(cardboards4))
+    
+    print("-"*30)
+    
+    cardboards5 = [(0,6,2),(0,6,3)]
+    print("Input 5:", cardboards5)
+    print("Output 5:", cardboard_intersection(cardboards5))
+    
+    print("-"*30)
+    
+    cardboards6 = [(0,6,3),(0,6,2)]
+    print("Input 6:", cardboards6)
+    print("Output 6:", cardboard_intersection(cardboards6))
+    
 
 
                 
